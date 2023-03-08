fff5fdf504b96a49c438fb148a562123865829e9
Shiv Nandan Jha <neureto12@gmail.com>
2023-03-08 11:31:29 +0530

    Reverse The Number

---
+++ Question/Ques1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+    /*
+  todo: reverse the order of the number input by the user
+*/
+
+int main(){
+    int number;
+    cout << "Enter the number: ";  // Prompt the user to enter a number
+    cin >> number;                 // Read the number from user input
+    int ans = 0;                   // Initialize the variable ans to 0
+    while(number!=0){              // Loop until all digits of the number are processed
+        int reverse = number % 10; // Extract the last digit of the number using modulus operator
+        ans = ans * 10 + reverse;  // Reverse the number by adding the extracted digit to ans after multiplying it by 10
+        number /= 10;              // Remove the last digit from the number using integer division
+    }
+    cout << "Reversed number is: " << ans << endl;  // Print the reversed number
+}
