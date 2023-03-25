e34307ec9dcc74699e13ae7b571109af9d8bcdac
Shiv Nandan Jha <neureto12@gmail.com>
2023-03-25 09:28:24 +0530

    New Question adding

--- Question/reverseNum.cpp
+++ Question/reverseNum.cpp
@@ -11,7 +11,11 @@ int main(){
     cin >> number;                 // Read the number from user input
     int ans = 0;                   // Initialize the variable ans to 0
     while(number!=0){              // Loop until all digits of the number are processed
-        int reverse = number % 10; // Extract the last digit of the number using modulus operator
+        int reverse = number % 10;  // Extract the last digit of the number using modulus operator
+        if((ans> INT16_MAX/10)||(ans<INT16_MIN/10)){
+            return 0;
+        }
+
         ans = (ans * 10) + reverse;  // Reverse the number by adding the extracted digit to ans after multiplying it by 10
         number /= 10;              // Remove the last digit from the number using integer division
     }
